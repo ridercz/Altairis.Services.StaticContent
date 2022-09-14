bc433633440297138d9e151c5876395fc350058e
Michal Altair Valášek <github@rider.cz>
2022-09-14 20:21:02 +0200

    Code cleanup.

--- Altairis.Services.StaticContent.DemoApp/Pages/Privacy.cshtml.cs
+++ Altairis.Services.StaticContent.DemoApp/Pages/Privacy.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Altairis.Services.StaticContent.DemoApp.Pages;
 public class PrivacyModel : PageModel {

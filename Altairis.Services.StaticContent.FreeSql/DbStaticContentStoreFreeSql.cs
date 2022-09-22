d02771a7b5b2adb179399e31f9a6e3eb4d1029ac
Jiri Tuma <Olbrasoft@mail.tj>
2022-09-22 16:58:01 +0200

    TDD Add class for registration to the DI

--- Altairis.Services.StaticContent.FreeSql/DbStaticContentStoreFreeSql.cs
+++ Altairis.Services.StaticContent.FreeSql/DbStaticContentStoreFreeSql.cs
@@ -30,4 +30,7 @@ public class DbStaticContentStoreFreeSql : IStaticContentStore
         }
         return items.Single().Text;
     }
+
+
+
 }
---
+++ Altairis.Services.StaticContent.FreeSql/StaticContentBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Altairis.Services.StaticContent;
+
+public static class StaticContentBuilderExtensions
+{
+    public static object UseDbStaticContentStoreFreeS( this StaticContentBuilder builder)
+    {
+        builder.Services.AddTransient<IStaticContentStore, DbStaticContentStoreFreeSql>();
+
+        return builder;
+    }
+}

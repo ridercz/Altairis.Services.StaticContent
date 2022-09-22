8d9977f3fd4385d4b3d84dae667d0fabbf070766
Jiri Tuma <Olbrasoft@mail.tj>
2022-09-22 17:24:53 +0200

    Add pragma warning disable

--- Altairis.Services.StaticContent.FreeSql.Tests/DbStaticContentStoreFreeSqlTests.cs
+++ Altairis.Services.StaticContent.FreeSql.Tests/DbStaticContentStoreFreeSqlTests.cs
@@ -65,7 +65,9 @@ public class DbStaticContentStoreFreeSqlTests
 
         //Assert
 
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
         Assert.Throws<ArgumentNullException>(() => new DbStaticContentStoreFreeSql(freeSqlMock.Object, null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
     }
 
 

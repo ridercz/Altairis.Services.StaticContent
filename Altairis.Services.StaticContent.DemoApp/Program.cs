9895e959a2970e45091c94a09ef5778bc6e325a6
Jiri Tuma <Olbrasoft@mail.tj>
2022-09-22 22:52:32 +0200

    Add FreeSql setting to DemoApp

--- Altairis.Services.StaticContent.DemoApp/Program.cs
+++ Altairis.Services.StaticContent.DemoApp/Program.cs
@@ -10,16 +10,42 @@ builder.Services.AddDbContext<DemoDbContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("StaticContent"));
 });
 
+#region for use DbStaticContentStoreFreeSql
+
+
+//Uncoment for use DbStaticContentStoreFreeSql
+IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+    .UseConnectionString(FreeSql.DataType.Sqlite, builder.Configuration.GetConnectionString("StaticContent"))
+    //Automatically synchronize the entity structure to the database.
+    //FreeSql will not scan the assembly, and will generate a table if and only when the CRUD instruction is executed.
+    .UseAutoSyncStructure(false)
+    .Build();
+
+builder.Services.AddSingleton(fsql);
+
+fsql.CodeFirst // https://www.cnblogs.com/FreeSql/p/11531302.html
+    .ConfigEntity<StaticContentItem>(a =>
+    {
+        a.Name("StaticContentItems");
+
+    });
+
+
+#endregion
+
 // Add static content services
 builder.Services.Configure<StaticContentViewComponentOptions>(options => {
     options.SurroundingElementName = "div";
     options.SurroundingElementAttributes.Add("class", "static");
     options.SurroundingElementAttributes.Add("title", "Firma \"Ďábel & syn\".");
 });
+
+
 builder.Services.AddStaticContent()
     .WithMarkdownFormatter()
-    .WithDbStaticContentStore<DemoDbContext>();
-    //.WithFileStaticContentStore("./App_Data/Content");
+    .UseDbStaticContentStoreFreeSql(); //Uncoment for use DbStaticContentStoreFreeSql
+ //.WithDbStaticContentStore<DemoDbContext>();
+ //.WithFileStaticContentStore("./App_Data/Content");
 
 // Add Razor Pages
 builder.Services.AddRazorPages();
